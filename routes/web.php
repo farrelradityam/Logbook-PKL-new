5d9b311cb7b8126bc0c17f35b52073918465285f
farrelradityam <farrelraditya869@gmail.com>
2024-08-13 13:06:53 +0700

    feat:menjawab 4 soal query erd

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,59 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     dump('Bagaimana caranya mendapatkan data berikut: Sekolah X pernah terdaftar PKL pada Tahun/Angkatan berapa saja');
+
+    $data = DB::table('schools')
+        ->join('batch_schools', 'schools.id', '=', 'batch_schools.school_id')
+        ->join('batches', 'batch_schools.batch_id', '=', 'batches.id')
+        ->select('schools.name as school_name', 'batches.year as batch_year')
+        ->where('schools.name', 'SMKN 10 Tarakan')
+        ->get();
+    
+    dump($data);
+    
+
     dump('Bagaimana caranya mendapatkan data berikut: Pada Tahun/Angkatan 2024, Sekolah mana saja yang mendaftar PKL?');
+
+    $data = DB::table('schools')
+        ->join('batch_schools', 'schools.id', '=', 'batch_schools.school_id')
+        ->join('batches', 'batch_schools.batch_id', '=', 'batches.id')
+        ->select('schools.name as school_name', 'batches.year as batch_year')
+        ->where('batches.year', '2024')
+        ->get();
+
+    dump($data);
+
+
     dump('Bagaimana caranya mendapatkan data berikut: Jurusan apa saja yang terdaftar PKL dari Sekolah X pada Tahun/Angkatan 2024?');
+
+    $data = DB::table('schools')
+        ->join('batch_schools', 'schools.id', '=', 'batch_schools.school_id')
+        ->join('batches', 'batch_schools.batch_id', '=', 'batches.id')
+        ->join('batch_school_majors', 'batch_school_id', '=', 'batch_school_majors.batch_school_id')
+        ->join('majors', 'batch_school_majors.major_id', '=', 'majors.id')
+        ->select('majors.code as major_code')
+        ->where('schools.name', 'SMK 2 Sawahlunto')
+        ->where('batches.year', '1973')
+        ->get();
+
+    dump($data);
+
     dump('Bagaimana caranya mendapatkan data berikut: Sekolah mana saja yang terdaftar PKL pada Tahun/Angkatan 2024 dan memiliki Jurusan PPLG?');
-    // return view('welcome',['title' =>'home page']);
+
+    $data = DB::table('schools')
+        ->join('batch_schools', 'schools.id', '=', 'batch_schools.school_id')
+        ->join('batches', 'batch_schools.batch_id', '=', 'batches.id')
+        ->join('batch_school_majors', 'batch_school_id', '=', 'batch_school_majors.batch_school_id')
+        ->join('majors', 'batch_school_majors.major_id', '=', 'majors.id')
+        ->select('schools.name as school_name')
+        ->where('batches.year', '1973')
+        ->where('majors.code', 'WST')
+        ->get();
+
+    dump($data);
+
+
+        // return view('welcome',['title' =>'home page']);
 });
 
 Route::get('/calendar', function () {
